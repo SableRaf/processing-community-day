c528920afd3aadadba3c6434332ae7022b419186
Raphaël de Courville <groupes.raphael@gmail.com>
2026-03-17 13:03:11 +0100

    Fix link to submit event

--- pcd-website/src/config.ts
+++ pcd-website/src/config.ts
@@ -1,6 +1,6 @@
 export const PCD_EMAIL = "day@processingfoundation.org";
 export const SUBMIT_EVENT_URL =
-  "https://github.com/processing/processing-community-day/issues/new?template=new-event.yml";
+  "https://github.com/processing/processing-community-day/issues/new?template=01-new-event.yml";
 export const GITHUB_EVENTS_BASE_URL =
   "https://github.com/processing/processing-community-day/tree/main/pcd-website/src/content/events";
 export const GITHUB_CONTENT_ISSUE_URL =
