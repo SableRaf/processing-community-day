813234acae51def2c4029f0f784042e4c04cf349
Raphaël de Courville <groupes.raphael@gmail.com>
2026-08-13 19:48:12 +0200

    Remove PF from sponsor list

--- pcd-website/src/config/sponsors.ts
+++ pcd-website/src/config/sponsors.ts
@@ -5,5 +5,4 @@ export interface Sponsor {
 
 export const SPONSORS: Sponsor[] = [
   { name: 'OpenProcessing', href: 'https://openprocessing.org' },
-  { name: 'Processing Foundation', href: 'https://processingfoundation.org' },
 ];
