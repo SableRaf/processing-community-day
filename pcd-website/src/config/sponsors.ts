c37191a586fe091a52b3bb99de6e6be842cae7fc
Raphaël de Courville <groupes.raphael@gmail.com>
2026-08-24 17:44:49 +0200

    Add OpenProcessing SVG logo

--- pcd-website/src/config/sponsors.ts
+++ pcd-website/src/config/sponsors.ts
@@ -1,8 +1,16 @@
+import type { ImageMetadata } from 'astro';
+import openProcessingLogo from '../images/openprocessing_logo.svg';
+
 export interface Sponsor {
   name: string;
   href: string;
+  logo: ImageMetadata;
 }
 
 export const SPONSORS: Sponsor[] = [
-  { name: 'OpenProcessing', href: 'https://openprocessing.org' },
+  {
+    name: 'OpenProcessing',
+    href: 'https://openprocessing.org',
+    logo: openProcessingLogo,
+  },
 ];
